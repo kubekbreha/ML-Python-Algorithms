5a8bdf5f6a8d9c4d766801326b420a101154a888
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-19 16:14:36 +0200

    best accuracy svm add

--- svm/project/svm_author_id.py
+++ svm/project/svm_author_id.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python
 
 """
-    This is the code to accompany the Lesson 2 (SVM) mini-project.
-    Use a SVM to identify emails from the Enron corpus by their authors:
+    this is the code to accompany the Lesson 2 (SVM) mini-project
+    use an SVM to identify emails from the Enron corpus by their authors
+
     Sara has label 0
     Chris has label 1
 """
 
+import sys
 from time import time
-
+sys.path.append("../tools/")
 from email_preprocess import preprocess
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
-
-#########################################################
-### your code goes here ###
-
 from sklearn import svm
-
-clf = svm.SVC()
-
-
-t = time()
-print(clf.fit(features_train, labels_train))
-print("training time:", round(time() - t, 3), "s")
+from sklearn.metrics import accuracy_score
 
 
+features_train, features_test, labels_train, labels_test = preprocess()
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+clf = svm.SVC(kernel="rbf", C=11000)
 t0 = time()
-print(clf.predict(features_test))
-print("predict time:", round(time() - t0, 3), "s")
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+prediction = clf.predict(features_test)
+print "Prediction: ", prediction
+print "Prediction time:", round(time()-t1, 3), "s"
 
-print(clf.score(features_test, labels_test))
+print accuracy_score(prediction, labels_test)
 
+chris = []
+for i in prediction:
+    if i == 1:
+        chris.append(i)
 
-
-
-#########################################################
+print len(chris)
