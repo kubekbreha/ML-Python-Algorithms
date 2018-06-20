d258916ffd48cab588e1468ebb49e2486b837a11
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-20 20:46:06 +0200

    decision three complete

--- decision_threes/project/dt_author_id.py
+++ decision_threes/project/dt_author_id.py
@@ -8,7 +8,7 @@
 from time import time
 
 from email_preprocess import preprocess
-from sklearn import svm
+from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 
@@ -18,7 +18,8 @@ from sklearn.metrics import accuracy_score
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-clf = svm.DecisionTreeClassifier(min_samples_split=40)
+print len(features_train[0])
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
 t0 = time()
 clf.fit(features_train, labels_train)
@@ -31,13 +32,13 @@ print "Prediction time:", round(time()-t1, 3), "s"
 
 print accuracy_score(prediction, labels_test)
 
-# chris = []
-# for i in prediction:
-#     if i == 1:
-#         chris.append(i)
-#
-# print len(chris)
-#
-# print prediction[10]
-# print prediction[26]
-# print prediction[50]
+chris = []
+for i in prediction:
+    if i == 1:
+        chris.append(i)
+
+print len(chris)
+
+print prediction[10]
+print prediction[26]
+print prediction[50]
