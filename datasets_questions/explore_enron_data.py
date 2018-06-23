6d5cb2bfcefb1821f658bd7297acb1932f4cf883
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-23 12:07:22 +0200

    20 data excercise

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,36 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-print len(enron_data)
+# How many data points (people) are in the dataset?
+len(enron_data)
+len(enron_data.keys())
+
+# For each person, how many features are available?
+len(enron_data.values()[0])
+enron_data[enron_data.keys()[0]]
+len(enron_data['SKILLING JEFFREY K'])
+
+# How many POIs are there in the E+F dataset
+# Example 1:
+count = 0
+for i in range(len(enron_data)):
+    a = enron_data.values()
+    if a[i]['poi'] == True:
+        count = count + 1
+print count
+
+# How Many POIs Exist?
+poi_text = '../final_project/poi_names.txt'
+poi_names = open(poi_text, 'r')
+fr = poi_names.readlines()
+print len(fr[2:])
+poi_names.close()
+
+# What is the total value of the stock belonging to James Prentice?
+print enron_data['PRENTICE JAMES']['total_stock_value']
+
+# How many email messages do we have from Wesley Colwell to persons of interest?
+print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+# Whats the value of stock options exercised by Jeffrey K Skilling?
+print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
