7baf2d29ff2b6fec1f61e3fc730f96f2b6309c68
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-25 12:57:12 +0200

    kmeans section done

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -4,17 +4,13 @@
     Skeleton code for k-means clustering mini-project.
 """
 
-
-
-
 import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
-
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -24,7 +20,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     ### drawing more than five clusters
     colors = ["b", "c", "k", "m", "g"]
     for ii, pp in enumerate(pred):
-        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+        plt.scatter(features[ii][0], features[ii][1], color=colors[pred[ii]])
 
     ### if you like, place red stars over points that are POIs (just for funsies)
     if mark_poi:
@@ -37,36 +33,53 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.show()
 
 
-
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 ### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
-
 
 ### the input features we want to use
 ### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
+# feature_3 = "total_payments"
+poi = "poi"
 features_list = [poi, feature_1, feature_2]
-data = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data )
+data = featureFormat(data_dict, features_list)
+poi, finance_features = targetFeatureSplit(data)
 
+max = 0
+for i in range(len(data)):
+    if finance_features[i][0] > max:
+        max = finance_features[i][0]
+print max
+
+
+min = 10000000
+for i in range(len(data)):
+    if finance_features[i][0] < min and finance_features[i][0]!=0:
+        min = finance_features[i][0]
+print min
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+    plt.scatter(f1, f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
+import numpy as np
 
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(finance_features)
+pred = kmeans.predict(finance_features)
 
-
+print kmeans.labels_
+print kmeans.cluster_centers_
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
